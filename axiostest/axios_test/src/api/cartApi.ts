d7b4e6e8f6b6a5120bcc88b53ce742e59cd44cfa
nacho-0 <iridium2827@gmail.com>
2026-06-23 08:37:57 +0900

    연동

---
+++ axiostest/axios_test/src/api/cartApi.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+type CartItemRequest = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+export const addCartItem = async (item: CartItemRequest) => {
+  const res = await axios.post(
+    "https://jsonplaceholder.typicode.com/posts",
+    item
+  );
+
+  return res.data;
+};
